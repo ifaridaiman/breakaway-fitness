868bc181546e33d8927ee30bcdcc7968bc9bda33
Nadim Hairi <nadimhairi@hotmail.com>
2025-07-21 19:44:01 +0800

    remove hover stop auto scroll image

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <img
-        className="absolute -right-3/5 md:-right-2/3 lg:-right-1/2 -z-10 top-0 mt-16 md:mt-24 w-[1000px]"
+        className="absolute -right-3/5 md:-right-2/3 lg:-right-1/2 -z-10 top-0 mt-16 md:mt-24 w-auto"
         src="/assets/rectangle-line-3.svg"
       />
       <HeroSection />
