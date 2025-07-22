6a5336900b2ec246e4d84bbf0c365d00875cf4e1
Nadim Hairi <nadimhairi@hotmail.com>
2025-07-22 18:34:09 +0800

    adding fonts as requested

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,11 +1,23 @@
+import { cn } from "@/lib/utils";
 import MaxWidthWrapper from "../MaxWidthWrapper";
 import HeroImages from "./HeroImages";
+import localFont from "next/font/local";
+
+const impact = localFont({
+  src: "../../public/fonts/Impact-Font/impact.ttf",
+  display: "swap",
+});
+
+const madeOuterSans = localFont({
+  src: "../../public/fonts/MADE-Outer-Sans-Black-PERSONAL-USE.otf",
+  display: "swap",
+});
 
 const HeroSection = () => {
   return (
-    <div className="space-y-10 mt-8">
+    <div className={cn("space-y-10 mt-8", madeOuterSans.className)}>
       <MaxWidthWrapper>
-        <h3 className="font-black text-5xl sm:text-6xl md:text-7xl lg:text-9xl xl:text-[8.8rem] xl:leading-32">
+        <h3 className="font-black text-4xl sm:text-6xl md:text-7xl lg:text-8xl 2xl:text-[8.5rem] 2xl:leading-32">
           BREAK FREE. <br />
           BREAK THROUGH. <br />
           <span className="bg-gradient-to-tr from-lime-400 from-15% to-emerald-500 to-65% bg-clip-text text-transparent">
