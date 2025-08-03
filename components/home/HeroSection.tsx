b65b808598ab668692c5d9326f93cc9c080b1453
Nadim Hairi <nadimhairi@hotmail.com>
2025-08-04 07:59:10 +0800

    adjust hero section

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,18 +1,11 @@
-import { cn } from "@/lib/utils";
-import MaxWidthWrapper from "../MaxWidthWrapper";
-import HeroImages from "./HeroImages";
-import localFont from "next/font/local";
-
-const madeOuterSans = localFont({
-  src: "../../public/fonts/MADE-Outer-Sans-Black-PERSONAL-USE.otf",
-  display: "swap",
-});
+import MaxWidthWrapper from '../MaxWidthWrapper'
+import HeroImages from './HeroImages'
 
 const HeroSection = () => {
   return (
-    <div className={cn("space-y-10 mt-8", madeOuterSans.className)}>
-      <MaxWidthWrapper>
-        <h3 className="font-black text-4xl sm:text-6xl md:text-7xl lg:text-8xl 2xl:text-[8.5rem] 2xl:leading-32">
+    <div className="space-y-10 mt-8">
+      <MaxWidthWrapper className="flex items-center h-[50vh]">
+        <h3 className="font-black text-[7vw] leading-[0.9]">
           BREAK FREE. <br />
           BREAK THROUGH. <br />
           <span className="bg-gradient-to-tr from-lime-400 from-15% to-emerald-500 to-65% bg-clip-text text-transparent">
@@ -22,7 +15,7 @@ const HeroSection = () => {
       </MaxWidthWrapper>
       <HeroImages />
     </div>
-  );
-};
+  )
+}
 
-export { HeroSection };
+export { HeroSection }
