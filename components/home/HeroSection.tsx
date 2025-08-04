7c37a156ff5448ff70f23d8f59d5f6c4d1121bfb
Nadim Hairi <nadimhairi@hotmail.com>
2025-08-04 21:54:20 +0800

    udpate content

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -3,9 +3,9 @@ import HeroImages from './HeroImages'
 
 const HeroSection = () => {
   return (
-    <div className="space-y-10 mt-8">
-      <MaxWidthWrapper className="flex flex-col justify-center gap-[1.4rem] lg:h-[60vh]">
-        <h3 className="font-bold text-[8vw] leading-[0.85]">
+    <div className="space-y-10 mt-[4vh]">
+      <MaxWidthWrapper className="flex flex-col justify-center gap-[1.4rem] lg:h-[50vh]">
+        <h3 className="font-bold text-[10vw] md:text-[8vw] leading-[0.85]">
           <span className="bg-gradient-to-tr from-lime-400 from-15% to-emerald-500 to-65% bg-clip-text text-transparent">
             BREAK AWAY
           </span>{' '}
@@ -18,6 +18,7 @@ const HeroSection = () => {
       </MaxWidthWrapper>
       <div className="space-y-2">
         <HeroImages />
+        <HeroImages className="block md:hidden" speed={0.03} />
       </div>
     </div>
   )
